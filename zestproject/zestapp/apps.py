3654d59c7d2e3c920f48aaef74177d271afa9a02
leahgwin <leahgwin@gmail.com>
2019-10-24 13:47:27 -0500

    set up of project and addition of models

---
+++ zestproject/zestapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ZestappConfig(AppConfig):
+    name = 'zestapp'
---
+++ zestproject/zestapp/models/__init__.py
@@ -0,0 +1,8 @@
+from .recipe import Recipe
+from .recipe_ingredient import Recipe_Ingredient
+from .ingredient import Ingredient
+from .category import Category
+from .cuisine import Cuisine
+from .user_group import User_Group
+from .group import Group
+
